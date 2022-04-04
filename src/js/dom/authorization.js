423fcd5b17baa60d3478580039b69da2d8106f16
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-04-04 10:24:25 +0500

    Snackbar created

--- src/js/dom/authorization.js
+++ src/js/dom/authorization.js
@@ -2,35 +2,45 @@ const continuebutton = document.getElementById('continue');
 const submit = document.getElementById('submit');
 
 const submitListener = (event) => event.preventDefault();
-
-submit.addEventListener('click', submitListener);
-
-continuebutton.addEventListener('click', function () {
+let continuebuttonListenner = function () {
   const login = document.getElementById('login').value;
   const password = document.getElementById('password').value;
+  const snackbar = document.getElementById('snackbar');
+  const snackbarMessage = document.getElementById('snackbarMessage');
 
+  let errorMessage = null;
   if (!login || !password) {
     if (!login && !password) {
-      alert('Поля пароль и логин обязательны для заполнения');
+      errorMessage = message.allRequired;
     }
     if (!login && password) {
-      alert('Поле логин обязательно для заполнения');
+      errorMessage = message.loginRequired;
     }
     if (!password && login) {
-      alert('Поле пароль обязательно для заполнения');
+      errorMessage = message.passwordRequired;
     }
+    snackbar.style.display = 'block';
+    snackbarMessage.textContent = errorMessage;
   } else {
     const isValidPassword = Validation.validatePassword(password);
     if (isValidPassword) {
       const isValidData = Validation.validateInputData(login, password);
       if (isValidData) {
+        snackbar.style.display = 'none';
         StorageManager.authorize(login);
         submit.removeEventListener('click', submitListener);
       } else {
-        alert('Логин или пароль не существуют');
+        errorMessage = message.notExist;
+        snackbar.style.display = 'block';
+        snackbarMessage.textContent = errorMessage;
       }
     } else {
-      alert('Пароль должен иметь не менее 8 символов и не менее 2 цифр');
+      errorMessage = message.passwordRequirements;
+      snackbar.style.display = 'block';
+      snackbarMessage.textContent = errorMessage;
     }
   }
-});
+};
+
+submit.addEventListener('click', submitListener);
+continuebutton.addEventListener('click', continuebuttonListenner);
--- src/js/dom/modalWindow.js
+++ src/js/dom/modalWindow.js
@@ -10,3 +10,14 @@ document.getElementById('close').addEventListener('click', function () {
   const modalWindow = document.getElementById('modalWindow');
   modalWindow.classList.remove('modal-window_open');
 });
+
+const passwordType = document.getElementById('passwordType');
+passwordType.addEventListener('click', function () {
+  if (password.getAttribute('type') === 'password') {
+    password.setAttribute('type', 'text');
+    passwordType.setAttribute('src', '../assets/hide.png');
+  } else {
+    password.setAttribute('type', 'password');
+    passwordType.setAttribute('src', '../assets/open.png');
+  }
+});
