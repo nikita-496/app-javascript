c49fbffc0e50d1a5e7817dc06118276115965e46
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-04-05 14:39:51 +0500

    Redering application components due to the current state

--- src/js/dom/buttonStatus.js
+++ src/js/dom/buttonStatus.js
@@ -1,9 +1,32 @@
 const authorizedUser = StorageManager.getUser();
-const loginButton = document.getElementById('buttonHeader');
-const logOutButton = document.createElement('form');
-logOutButton.classList = 'form';
-logOutButton.action = './index.html';
-logOutButton.innerHTML = '<button class="button header__button" id="logout">Выйти</button>';
-if (authorizedUser) {
-  loginButton.parentNode.replaceChild(logOutButton, loginButton);
+const loginButtonHeader = document.getElementById('buttonHeader');
+const logOutWraapper = document.createElement('form');
+const goProfileWraapper = document.createElement('form');
+
+const loginButtonTitle = document.getElementById('btn');
+
+function changeViewButton() {
+  logOutWraapper.classList = 'form';
+  goProfileWraapper.classList = 'form';
+  changeButtonHeader();
+  changeButtonTitle();
 }
+
+function changeButtonHeader() {
+  logOutWraapper.action = './index.html';
+  logOutWraapper.innerHTML = '<button class="button header__button" id="logout">Выйти</button>';
+  if (authorizedUser) {
+    loginButtonHeader.parentNode.replaceChild(logOutWraapper, loginButtonHeader);
+  }
+}
+
+function changeButtonTitle() {
+  goProfileWraapper.action = './profile.html';
+  goProfileWraapper.innerHTML =
+    '<button class="button button__action-group button__action-group_theme_login button-login" id="btn">В кабинет</button>';
+  if (authorizedUser) {
+    loginButtonTitle.parentNode.replaceChild(goProfileWraapper, loginButtonTitle);
+  }
+}
+
+changeViewButton();
